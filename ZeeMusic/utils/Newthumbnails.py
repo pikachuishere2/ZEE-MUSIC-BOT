698ecdcc463b45a604d29776ae2ca8665efcabf8
Nottyboypro <nottyboy.vip.@gmail.com>
2025-09-22 21:38:02 +0530

    Update Newthumbnails.py

--- ZeeMusic/utils/Newthumbnails.py
+++ ZeeMusic/utils/Newthumbnails.py
@@ -53,7 +53,7 @@ def trim_to_width(text: str, font: ImageFont.FreeTypeFont, max_w: int) -> str:
     return ellipsis
 
 
-async def get_thumb(videoid: str) -> str:
+async def gen_thumb(videoid: str) -> str:
     cache_path = os.path.join(CACHE_DIR, f"{videoid}_v5.png")
     if os.path.exists(cache_path):
         return cache_path
