1c33155abcb456b01f1a37ac92df14abed55f2d9
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-09-30 08:16:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 999999))
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1003072931688"))
 
 # Get this value from @MissRose_Bot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "8383747125"))
+OWNER_ID = int(getenv("OWNER_ID", "8457068373"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -121,6 +121,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
