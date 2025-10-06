f5edd1b0e493baefd45f7a83752e17737566806e
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2025-10-06 16:23:09 +0530

    Update Youtube.py

--- ZeeMusic/platforms/Youtube.py
+++ ZeeMusic/platforms/Youtube.py
@@ -16,7 +16,7 @@ import random
 import logging
 import aiohttp
 import config
-from config import BASE_API_URL, API_KEY
+from config import API_URL, VIDEO_API_URL, API_KEY
 
 
 def cookie_txt_file():
@@ -40,7 +40,7 @@ async def download_song(link: str):
             #print(f"File already exists: {file_path}")
             return file_path
         
-    song_url = f"{BASE_API_URL}/song/{video_id}?api={API_KEY}"
+    song_url = f"{API_URL}/song/{video_id}?api={API_KEY}"
     async with aiohttp.ClientSession() as session:
         for attempt in range(10):
             try:
@@ -102,7 +102,7 @@ async def download_video(link: str):
         if os.path.exists(file_path):
             return file_path
         
-    video_url = f"{BASE_API_URL}/video/{video_id}?api={API_KEY}"
+    video_url = f"{VIDEO_API_URL}/video/{video_id}?api={API_KEY}"
     async with aiohttp.ClientSession() as session:
         for attempt in range(10):
             try:
@@ -596,4 +596,3 @@ class YouTubeAPI:
             direct = True
             downloaded_file = await download_song(link)
         return downloaded_file, direct
-
